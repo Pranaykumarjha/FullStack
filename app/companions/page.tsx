48b6fd5cce0ec79e3887e1b9532f9a20a4253879
Pranay Kumar Jha <pranayjha952@gmail.com>
2026-04-19 00:23:35 +0530

    Starting new Work

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,16 @@
+import { getAllCompanions } from "@/lib/actions/companion.actions";
 
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+ const companions = await getAllCompanions({subject,topic});
+ console.log(companions);
   return (
     <div>
-      
+
     </div>
   )
 }
