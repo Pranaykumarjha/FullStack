0ebeb6e5bbd54af31c1c60bcde54e81cf60200a8
Pranay Kumar Jha <pranayjha952@gmail.com>
2026-04-29 01:00:59 +0530

    Changed CompanionComp

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,8 +1,11 @@
 'use client'
 import React, { use } from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { cn, getSubjectColor } from '@/lib/utils'
-
+import { vapi } from '@/lib/vapi.sdk'
+import Image from 'next/image'
+import Lottie from 'lottie-react'
+import soundwaves from '@/constants/soundwaves.json'
 enum CallStatus {
     INACTIVE = 'INACTIVE',
     CONNECTING = 'CONNECTING',
@@ -12,12 +15,44 @@ enum CallStatus {
 
 const CompanionComponent = ({ companionId, subject, topic, name, userName, userImage, style, voice }: CompanionComponentProps) => {
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+    const [isSpeaking, setIsSpeaking] = useState(false);
+    const lottieRef = useRef<LottieRefCurrentProps>(null);
+    useEffect(() => {
+        if (lottieRef) {
+            if (isSpeaking) {
+                lottieRef.current?.play();
+            } else {
+                lottieRef.current?.stop();
+            }
+        }
+    }, [isSpeaking, lottieRef]);
     useEffect(() => {
         const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
 
         const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
 
-        const onMessage = {}
+        const onMessage = () => { };
+        const onSpeechStart = () => setIsSpeaking(true);
+        const onSpeechEnd = () => setIsSpeaking(false);
+
+
+        const onError = (error: Error) => console.error('WebSocket error:', error);
+
+        vapi.on('call-start', onCallStart);
+        vapi.on('call-end', onCallEnd);
+        vapi.on('message', onMessage);
+        vapi.on('error', onError);
+        vapi.on('speech-start', onSpeechStart);
+        vapi.on('speech-end', onSpeechEnd);
+
+        return () => {
+            vapi.off('call-start', onCallStart);
+            vapi.off('call-end', onCallEnd);
+            vapi.off('message', onMessage);
+            vapi.off('error', onError);
+            vapi.off('speech-start', onSpeechStart);
+            vapi.off('speech-end', onSpeechEnd);
+        };
     }, []);
 
     return (
@@ -28,9 +63,35 @@ const CompanionComponent = ({ companionId, subject, topic, name, userName, userI
                         className="companion-avatar"
                         style={{ backgroundColor: getSubjectColor(subject) }}
                     >
-                        <div className={cn('absolute transition-opacity duration-1000')}>
+                        <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE
+                            ? 'opacity-100'
+                            : 'opacity-0',
+                            callStatus === CallStatus.CONNECTING && 'opacity-100 animate-pulse')}>
+                            <Image src={`/icons/${subject}.svg`} alt={subject} width={150} height={150} className="max-sm:w-fit" />
+                            <div
+                                className={cn(
+                                    'absolute transition-opacity duration-1000',
+                                    callStatus === CallStatus.ACTIVE ? 'opacity-100' : 'opacity-0'
+                                )}
+                            >
+                                <Lottie
+                                    lottieRef={lottieRef}
+                                    animationData={soundwaves}
+                                    autoPlay={false}
+                                    className='companion-lottie' />
+
+                            </div>
                         </div>
+
                     </div>
+                    <p className='font-bold text-2xl'>{name}</p>
+                </div>
+                <div className='user-section'>
+                    <div className="user-avatar">
+                        <Image src={userImage} alt={userName} width={130} height={130} className="rounded-lg" />
+
+                    </div>
+
                 </div>
             </section>
         </section>
